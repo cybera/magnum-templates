b736d47e32e9c4f3427d11d643edaf57640673b9
Cybera Sysadmin <sysadmin@cybera.ca>
2020-04-02 03:34:44 +0000

    Pinning to Kubernetes 1.17

--- rac_magnum/templates/k8s-ubuntu/fragments/configure-kubernetes-minion.sh
+++ rac_magnum/templates/k8s-ubuntu/fragments/configure-kubernetes-minion.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update -qq
-sudo apt-get install -y kubectl kubelet kubeadm kubernetes-cni
+sudo apt-get install -y kubectl=1.17.0-00 kubelet=1.17.0-00 kubeadm=1.17.0-00 kubernetes-cni=0.7.5-00
 
 cat > /etc/default/kubelet <<EOF
 KUBELET_EXTRA_ARGS="--cloud-provider=external"
